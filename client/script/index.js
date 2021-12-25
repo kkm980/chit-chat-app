02f06ca0e94302516e4104eb4dc47e2128faa700
kkm980 <kantkrishna751@gmail.com>
2021-12-25 22:07:57 +0530

    user can send message

--- client/script/index.js
+++ client/script/index.js
@@ -1,10 +1,15 @@
 const chatInput = document.getElementById("chatInput");
+const msgBox = document.getElementById("msgBox");
 
 const socket =io();
 
 
-// here msg is the event and incoming is the message coming from socketio server upon any client addition or leave
-socket.on('msg', incoming=>{console.log(incoming)})
+// here msg is the event and incoming is the message coming from socketio server upon any client addition or leave or messaging
+socket.on('msg', incoming=>{
+    renderMsg(incoming);
+    msgBox.scrollTop = msgBox.scrollHeight;
+    
+})
 
 
 // function that triggers when user sends any message
@@ -15,4 +20,18 @@ chatInput.addEventListener('submit',(e)=>{
 
     // emits the message to the server 
     socket.emit('newMsg', e.target.elements.inpBox.value);
+
+    e.target.elements.inpBox.value="";
+    e.target.elements.inpBox.focus();
 })
+
+
+// appending the incoming message to client side 
+
+function renderMsg(incoming){
+   
+    const div=document.createElement('div');
+    div.innerHTML = `
+    <p>${incoming}</p>`;
+     msgBox.appendChild(div);
+}
