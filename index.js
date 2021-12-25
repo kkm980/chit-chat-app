5bf331ef8ce736a4a8fcf9b25f7d854f6519b5e1
kkm980 <kantkrishna751@gmail.com>
2021-12-25 16:54:30 +0530

    client side connected with server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const path = require('path')
+
 const express = require('express');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
 const port =3000;
 app.listen(port, ()=>{console.log("listenings")})
