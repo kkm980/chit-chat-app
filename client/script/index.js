c6e3670e6435a425ab83cf63ba175d5f1db5b44f
kkm980 <kantkrishna751@gmail.com>
2021-12-26 11:40:43 +0530

    js file changed and debugged

--- client/script/index.js
+++ client/script/index.js
@@ -1,48 +1,105 @@
-const chatInput = document.getElementById("chatInput");
-const msgBox = document.getElementById("msgBox");
 
 
-// getting room name and user name 
-const {Usrname, room} = Qs.parse(location.search, {
-    ignoreQueryPrefix: true
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// getting room name and username
+
+const { username, room } = Qs.parse(location.search,
+{
+  ignoreQueryPrefix: true,
 });
-console.log(Usrname, room);
+
+const socket = io();
+
+// function triggers when someone joins the room
+
+socket.emit('joinRoom',
+ { username, room }
+);
+
+// getting users and room name
+
+socket.on('roomUsers', ({ room, users })=>{
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 
-const socket =io();
+// receiving the message from socket server
 
+socket.on('message',(message)=>{
+  outputMessage(message);
 
-// here msg is the event and incoming is the message coming from socketio server upon any client addition or leave or messaging
-socket.on('msg', incoming=>{
-    renderMsg(incoming);
-    msgBox.scrollTop = msgBox.scrollHeight;
-    
-})
+  // Scroll down automatically when someone joins
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 
+});
 
-// function that triggers when user sends any message
-chatInput.addEventListener('submit',(e)=>{
+// function calls upon clicking the send button
+chatForm.addEventListener('submit',(e)=>
+{
+  e.preventDefault();
 
-    // to stop the default behaviour of form submission
-    e.preventDefault();
+  // getting message
 
-    // emits the message to the server 
-    socket.emit('newMsg', e.target.elements.inpBox.value);
+  let msg = e.target.elements.msg.value; 
+  
+  //   if the message is trailing spaces, do nothing 
+  msg = msg.trim();
+  if (!msg){
+    return false;
+  }
 
-    e.target.elements.inpBox.value="";
-    e.target.elements.inpBox.focus();
-})
+  // sending the message to socket server
+  socket.emit('chatMessage', msg);
 
+  // upon sending the message, input box value is empty string
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
 
-// appending the incoming message to client side 
-
-function renderMsg(incoming){
-   console.log(incoming);
-    const div=document.createElement('div');
-    div.innerHTML = `
-    <span>${incoming.time}</span>
-    <span>${incoming.name}</span>
-    <h5>${incoming.msg}</h5>`
-    
-     msgBox.appendChild(div);
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
 }
+
+// adding the room name dynamically to the dom
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// appending the available users in the room to dom
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+//throw alert when user clicks on leave the room in form of prompt to take final permission from user
+
+document.getElementById('leave-btn').addEventListener('click', () =>
+{
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+}
+);
